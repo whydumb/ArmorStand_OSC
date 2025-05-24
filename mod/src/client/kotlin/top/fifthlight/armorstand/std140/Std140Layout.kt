f4b918734c69d52a4745ece1209f20158c2e1f84
fifth_light <liu-yu-jie@foxmail.com>
2025-05-24 18:38:58 +0800

    简化 std140 布局辅助类中的泛型

--- mod/src/client/kotlin/top/fifthlight/armorstand/std140/Std140Layout.kt
+++ mod/src/client/kotlin/top/fifthlight/armorstand/std140/Std140Layout.kt
@@ -16,14 +16,14 @@ abstract class Std140Layout<L : Std140Layout<L>> {
     val totalSize: Int
         get() = offset
 
-    sealed class Field<T, L : Std140Layout<L>>(layout: Std140Layout<L>, align: Int, size: Int) {
+    sealed class Field<T>(layout: Std140Layout<*>, align: Int, size: Int) {
         val offset: Int
 
-        operator fun setValue(thisRef: Std140Layout<L>, property: KProperty<*>, value: T) {
+        operator fun setValue(thisRef: Std140Layout<*>, property: KProperty<*>, value: T) {
             setValue(checkNotNull(thisRef.buffer) { "No buffer bound to layout" }, value)
         }
 
-        operator fun getValue(thisRef: Std140Layout<L>, property: KProperty<*>): T =
+        operator fun getValue(thisRef: Std140Layout<*>, property: KProperty<*>): T =
             getValue(checkNotNull(thisRef.buffer) { "No buffer bound to layout" })
 
         abstract fun setValue(buffer: ByteBuffer, value: T)
@@ -38,7 +38,7 @@ abstract class Std140Layout<L : Std140Layout<L>> {
             layout.offset += padding + size
         }
 
-        class IntField<L : Std140Layout<L>>(layout: Std140Layout<L>) : Field<Int, L>(
+        class IntField(layout: Std140Layout<*>) : Field<Int>(
             layout = layout,
             align = 4,
             size = 4,
@@ -50,7 +50,7 @@ abstract class Std140Layout<L : Std140Layout<L>> {
             override fun getValue(buffer: ByteBuffer) = buffer.getInt(offset)
         }
 
-        class FloatField<L : Std140Layout<L>>(layout: Std140Layout<L>) : Field<Float, L>(
+        class FloatField(layout: Std140Layout<*>) : Field<Float>(
             layout = layout,
             align = 4,
             size = 4,
@@ -62,7 +62,7 @@ abstract class Std140Layout<L : Std140Layout<L>> {
             override fun getValue(buffer: ByteBuffer) = buffer.getFloat(offset)
         }
 
-        class Vec2Field<L : Std140Layout<L>>(layout: Std140Layout<L>) : Field<Vector2fc, L>(
+        class Vec2Field(layout: Std140Layout<*>) : Field<Vector2fc>(
             layout = layout,
             align = 8,
             size = 8,
@@ -78,7 +78,7 @@ abstract class Std140Layout<L : Std140Layout<L>> {
             }
         }
 
-        class Vec3Field<L : Std140Layout<L>>(layout: Std140Layout<L>) : Field<Vector3fc, L>(
+        class Vec3Field(layout: Std140Layout<*>) : Field<Vector3fc>(
             layout = layout,
             align = 16,
             size = 12,
@@ -94,7 +94,7 @@ abstract class Std140Layout<L : Std140Layout<L>> {
             }
         }
 
-        class Vec4Field<L : Std140Layout<L>>(layout: Std140Layout<L>) : Field<Vector4fc, L>(
+        class Vec4Field(layout: Std140Layout<*>) : Field<Vector4fc>(
             layout = layout,
             align = 16,
             size = 16,
@@ -110,7 +110,7 @@ abstract class Std140Layout<L : Std140Layout<L>> {
             }
         }
 
-        class IVec2Field<L : Std140Layout<L>>(layout: Std140Layout<L>) : Field<Vector2ic, L>(
+        class IVec2Field(layout: Std140Layout<*>) : Field<Vector2ic>(
             layout = layout,
             align = 8,
             size = 8,
@@ -126,7 +126,7 @@ abstract class Std140Layout<L : Std140Layout<L>> {
             }
         }
 
-        class IVec3Field<L : Std140Layout<L>>(layout: Std140Layout<L>) : Field<Vector3ic, L>(
+        class IVec3Field(layout: Std140Layout<*>) : Field<Vector3ic>(
             layout = layout,
             align = 16,
             size = 12,
@@ -142,7 +142,7 @@ abstract class Std140Layout<L : Std140Layout<L>> {
             }
         }
 
-        class IVec4Field<L : Std140Layout<L>>(layout: Std140Layout<L>) : Field<Vector4ic, L>(
+        class IVec4Field(layout: Std140Layout<*>) : Field<Vector4ic>(
             layout = layout,
             align = 16,
             size = 16,
@@ -158,7 +158,7 @@ abstract class Std140Layout<L : Std140Layout<L>> {
             }
         }
 
-        class Mat2Field<L : Std140Layout<L>>(layout: Std140Layout<L>) : Field<Matrix2fc, L>(
+        class Mat2Field(layout: Std140Layout<*>) : Field<Matrix2fc>(
             layout = layout,
             align = 8,
             size = 16,
@@ -174,7 +174,7 @@ abstract class Std140Layout<L : Std140Layout<L>> {
             }
         }
 
-        class Mat3Field<L : Std140Layout<L>>(layout: Std140Layout<L>) : Field<Matrix3fc, L>(
+        class Mat3Field(layout: Std140Layout<*>) : Field<Matrix3fc>(
             layout = layout,
             align = 16,
             size = 48,
@@ -206,7 +206,7 @@ abstract class Std140Layout<L : Std140Layout<L>> {
             }
         }
 
-        class Mat4Field<L : Std140Layout<L>>(layout: Std140Layout<L>) : Field<Matrix4fc, L>(
+        class Mat4Field(layout: Std140Layout<*>) : Field<Matrix4fc>(
             layout = layout,
             align = 16,
             size = 64,
@@ -222,7 +222,7 @@ abstract class Std140Layout<L : Std140Layout<L>> {
             }
         }
 
-        class IntArray<L : Std140Layout<L>>(layout: Std140Layout<L>, private val length: Int) : Field<IntList, L>(
+        class IntArray(layout: Std140Layout<*>, private val length: Int) : Field<IntList>(
             layout = layout,
             align = 4,
             size = 4 * length,
@@ -257,7 +257,7 @@ abstract class Std140Layout<L : Std140Layout<L>> {
             override fun getValue(buffer: ByteBuffer) = list.also { it.buffer = buffer }
         }
 
-        class FloatArray<L : Std140Layout<L>>(layout: Std140Layout<L>, private val length: Int) : Field<FloatList, L>(
+        class FloatArray(layout: Std140Layout<*>, private val length: Int) : Field<FloatList>(
             layout = layout,
             align = 4,
             size = 4 * length,
@@ -292,10 +292,10 @@ abstract class Std140Layout<L : Std140Layout<L>> {
             override fun getValue(buffer: ByteBuffer) = list.also { it.buffer = buffer }
         }
 
-        class Vec2Array<L : Std140Layout<L>>(
-            layout: Std140Layout<L>,
+        class Vec2Array(
+            layout: Std140Layout<*>,
             private val length: Int
-        ) : Field<MutableList<Vector2fc>, L>(
+        ) : Field<MutableList<Vector2fc>>(
             layout = layout,
             align = 8,
             size = length * 8,
@@ -328,10 +328,10 @@ abstract class Std140Layout<L : Std140Layout<L>> {
             override fun getValue(buffer: ByteBuffer) = list.also { it.buffer = buffer }
         }
 
-        class Vec3Array<L : Std140Layout<L>>(
-            layout: Std140Layout<L>,
+        class Vec3Array(
+            layout: Std140Layout<*>,
             private val length: Int
-        ) : Field<MutableList<Vector3fc>, L>(
+        ) : Field<MutableList<Vector3fc>>(
             layout = layout,
             align = 16,
             size = length * 16,
@@ -364,10 +364,10 @@ abstract class Std140Layout<L : Std140Layout<L>> {
             override fun getValue(buffer: ByteBuffer) = list.also { it.buffer = buffer }
         }
 
-        class Vec4Array<L : Std140Layout<L>>(
-            layout: Std140Layout<L>,
+        class Vec4Array(
+            layout: Std140Layout<*>,
             private val length: Int
-        ) : Field<MutableList<Vector4fc>, L>(
+        ) : Field<MutableList<Vector4fc>>(
             layout = layout,
             align = 16,
             size = length * 16,
