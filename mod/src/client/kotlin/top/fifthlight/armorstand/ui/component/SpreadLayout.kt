dcfe3d10f61eb0a4a90b2d278ab307f2d367c96e
fifth_light <liu-yu-jie@foxmail.com>
2025-06-13 11:58:52 +0800

    为 SpreadLayout 添加比例功能

--- mod/src/client/kotlin/top/fifthlight/armorstand/ui/component/SpreadLayout.kt
+++ mod/src/client/kotlin/top/fifthlight/armorstand/ui/component/SpreadLayout.kt
@@ -24,6 +24,7 @@ class SpreadLayout(
     private class Element<T : Widget>(
         private val inner: T,
         positioner: Positioner,
+        var weight: Int = 1,
         private val onSizeChanged: (widget: T, width: Int, height: Int) -> Unit = { _, _, _ -> },
     ) : WrappedElement(inner, positioner) {
         fun setSize(width: Int, height: Int) = onSizeChanged(
@@ -47,18 +48,26 @@ class SpreadLayout(
     fun <T : Widget> add(
         widget: T,
         positioner: Positioner = Positioner.create(),
+        weight: Int = 1,
         onSizeChanged: (widget: T, width: Int, height: Int) -> Unit
     ) {
-        elements.add(Element(widget, positioner, onSizeChanged))
+        elements.add(Element(widget, positioner, weight.coerceAtLeast(1), onSizeChanged))
     }
 
-    fun <T : ClickableWidget> add(widget: T, positioner: Positioner = Positioner.create()) {
-        add(widget, positioner) { w, width, height -> w.setDimensions(width, height) }
+    fun <T : ClickableWidget> add(
+        widget: T,
+        positioner: Positioner = Positioner.create(),
+        weight: Int = 1,
+    ) {
+        add(widget, positioner, weight) { w, width, height -> w.setDimensions(width, height) }
     }
 
-    fun <T> add(widget: T, positioner: Positioner = Positioner.create())
-            where T : ResizableLayout, T : Widget {
-        add(widget, positioner) { w, width, height -> w.setDimensions(width, height) }
+    fun <T> add(
+        widget: T,
+        positioner: Positioner = Positioner.create(),
+        weight: Int = 1,
+    ) where T : ResizableLayout, T : Widget {
+        add(widget, positioner, weight) { w, width, height -> w.setDimensions(width, height) }
     }
 
     override fun refreshPositions() {
@@ -66,42 +75,64 @@ class SpreadLayout(
         if (count == 0) return
 
         when (direction) {
-            Direction.HORIZONTAL -> {
-                val horizontalPadding = padding.left + padding.right
-
-                val availableWidth = width - horizontalPadding - gap * (count - 1)
-                val availableHeight = height - padding.top - padding.bottom
-
-                val elementWidth = availableWidth / count
-
-                var currentX = x + padding.left
-                elements.forEach { element ->
-                    element.setSize(elementWidth, availableHeight)
-                    element.setX(currentX, currentX + elementWidth)
-                    element.setY(y + padding.top, y + height - padding.bottom)
-
-                    currentX += elementWidth + gap
-                }
-            }
-            Direction.VERTICAL -> {
-                val verticalPadding = padding.top + padding.bottom
-
-                val availableWidth = width - padding.left - padding.right
-                val availableHeight = height - verticalPadding - gap * (count - 1)
-
-                val elementHeight = availableHeight / count
-
-                var currentY = y + padding.top
-                elements.forEach { element ->
-                    element.setSize(availableWidth, elementHeight)
-                    element.setX(x + padding.left, x + width - padding.right)
-                    element.setY(currentY, currentY + elementHeight)
-
-                    currentY += elementHeight + gap
-                }
-            }
+            Direction.HORIZONTAL -> refreshHorizontal()
+            Direction.VERTICAL -> refreshVertical()
         }
 
         super.refreshPositions()
+    }
+
+    private fun refreshHorizontal() {
+        val count = elements.size
+        val horizontalPadding = padding.left + padding.right
+        val availableWidth = width - horizontalPadding - gap * (count - 1)
+        val availableHeight = height - padding.top - padding.bottom
+
+        val totalWeight = elements.sumOf { it.weight }
+        val baseSize = availableWidth / totalWeight
+
+        var remainingSpace = availableWidth - baseSize * totalWeight
+        var currentX = x + padding.left
+
+        elements.forEach { element ->
+            var elementWidth = baseSize * element.weight
+            if (remainingSpace > 0) {
+                elementWidth += 1
+                remainingSpace -= 1
+            }
+
+            element.setSize(elementWidth, availableHeight)
+            element.setX(currentX, currentX + elementWidth)
+            element.setY(y + padding.top, y + height - padding.bottom)
+
+            currentX += elementWidth + gap
+        }
+    }
+
+    private fun refreshVertical() {
+        val count = elements.size
+        val verticalPadding = padding.top + padding.bottom
+        val availableWidth = width - padding.left - padding.right
+        val availableHeight = height - verticalPadding - gap * (count - 1)
+
+        val totalWeight = elements.sumOf { it.weight }
+        val baseSize = availableHeight / totalWeight
+
+        var remainingSpace = availableHeight - baseSize * totalWeight
+        var currentY = y + padding.top
+
+        elements.forEach { element ->
+            var elementHeight = baseSize * element.weight
+            if (remainingSpace > 0) {
+                elementHeight += 1
+                remainingSpace -= 1
+            }
+
+            element.setSize(availableWidth, elementHeight)
+            element.setX(x + padding.left, x + width - padding.right)
+            element.setY(currentY, currentY + elementHeight)
+
+            currentY += elementHeight + gap
+        }
     }
 }
